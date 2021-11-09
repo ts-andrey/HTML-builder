4b066ea56174c020e9a49cb18b3fcc949f4ac5d0
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2021-11-09 21:58:10 +0300

    fix: fix #3 task file names output issue

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -9,14 +9,13 @@ function getFiles(folder) {
   fs.readdir(folder, { withFileTypes: true }, (err, subs) => {
     if (err) return err;
     subs.forEach(sub => {
-      if (sub.isDirectory()) getFiles(path.join(folder, sub.name));
+      if (sub.isDirectory());
       else {
         const [name, ext] = sub.name.split('.');
         fs.stat(path.join(folder, sub.name), (err, stats) => {
           if (err) return err;
           let size = stats.size;
-          size = size < 10 ** 3 ? `${size}b` : size > 10 ** 6 ? `${size / 10 ** 6}mb` : `${size / 10 ** 3}kb`;
-          console.log(`${name} - ${ext} - ${size}`);
+          console.log(`${name} - ${ext} - ${size}b`);
         });
       }
     });
