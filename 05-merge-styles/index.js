4d31ecca535d1f6e270a2095c994463a4bf128cd
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2021-11-10 17:50:09 +0300

    refactor: change functionality to async/await in order to reduce errors

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,79 +1,69 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs/promises');
 
 const toDirPath = path.dirname(__dirname);
 const dirPath = '05-merge-styles';
 const stylesPath = 'styles';
 const resultPath = 'project-dist';
 
-const dir = path.join(toDirPath, dirPath);
+const styleDir = path.join(toDirPath, dirPath, stylesPath);
+const finalDir = path.join(toDirPath, dirPath, resultPath);
 
-let pathArr = [];
-let stylesSize = 0;
-let finalSize = 0;
+async function setInitialState() {
+  try {
+    await fs.rm(path.join(finalDir, 'bundle.css'), { recursive: true, force: true, maxRetries: 9 }, err => err);
+  } catch (err) {
+    return err;
+  }
+}
 
-function fuseFiles(files, finalFile) {
-  const writeStream = fs.createWriteStream(finalFile, { encoding: 'binary', flags: 'a' });
-  files.forEach(el => {
-    const readStream = fs.createReadStream(el, { encoding: 'binary' });
-    readStream.on('readable', () => {
-      const data = readStream.read();
-      if (data) writeStream.write(`${data}`);
+async function getDirItems(dir) {
+  const names = [];
+  const isFolders = [];
+  try {
+    const items = await fs.readdir(dir, { withFileTypes: true });
+    items.map(el => {
+      names.push(el.name);
+      isFolders.push(el.isDirectory());
     });
-  });
+  } catch (err) {
+    return err;
+  }
+  return [names, isFolders];
 }
 
-function getFiles(folderPath) {
-  let foldersName = [];
-  fs.readdir(folderPath, { withFileTypes: true }, (err, items) => {
-    if (err) return err;
-    for (let i = 0; i < items.length; i++) {
-      if (items[i].isDirectory()) {
-        foldersName.push(items[i].name);
-      } else {
-        if (items[i].name.split('.')[1] === 'css') {
-          const elPath = path.join(folderPath, items[i].name);
-          pathArr.push(elPath);
-          fs.stat(elPath, (err, stats) => {
-            if (err) return err;
-            stylesSize += stats.size;
-          });
-        }
+async function writeData(readPath, writePath) {
+  try {
+    const data = await fs.readFile(readPath, { encoding: 'binary' });
+    await fs.writeFile(writePath, data, { encoding: 'binary', flag: 'a' });
+  } catch (err) {
+    return err;
+  }
+}
+
+async function replicate(item) {
+  try {
+    const [styleItems, styleTypes] = await getDirItems(item);
+    const finalPath = path.join(finalDir, 'bundle.css');
+    for (let i = 0; i < styleItems.length; i++) {
+      const itemPath = path.join(item, styleItems[i]);
+      let ext = styleItems[i].split('.');
+      if (ext[ext.length - 1] === 'css') {
+        if (!styleTypes[i]) await writeData(itemPath, finalPath);
       }
+      if (styleTypes[i]) await replicate(itemPath);
     }
-
-    if (foldersName.length > 0) {
-      for (let i = 0; i < foldersName.length; i++) {
-        const nextPath = path.join(folderPath, foldersName[i]);
-        getFiles(nextPath);
-      }
-    }
-  });
+  } catch (err) {
+    return err;
+  }
 }
 
-function checkFinalSize(path) {
-  fs.stat(path, (err, stats) => {
-    if (err) return err;
-    finalSize = stats.size;
-  });
+async function merge() {
+  try {
+    await setInitialState();
+    await replicate(styleDir);
+  } catch (err) {
+    return err;
+  }
 }
-
-async function mergeStyles() {
-  const jobPath = path.join(dir, stylesPath);
-  const finalPath = path.join(dir, resultPath, 'bundle.css');
-
-  getFiles(jobPath);
-  checkFinalSize(finalPath);
-
-  setTimeout(() => {
-    if (finalSize === 0) {
-      console.log('created');
-      fuseFiles(pathArr, finalPath);
-    } else if (finalSize - stylesSize !== 0) {
-      console.log('rewrited');
-      fs.unlink(finalPath, err => err);
-      fuseFiles(pathArr, finalPath);
-    }
-  }, 50);
-}
-mergeStyles();
+merge();
