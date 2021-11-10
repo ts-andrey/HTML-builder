581ae495343db3b2d84919fea49e6926f6e587b6
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2021-11-10 14:26:59 +0300

    refactor: change logic to async in order to minimize errors

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs/promises');
 
 const toDirPath = path.dirname(__dirname);
 const dirPath = '06-build-page';
@@ -10,167 +10,108 @@ const componentFolder = 'components';
 
 const dir = path.join(toDirPath, dirPath);
 
-//После завершения работы скрипта должна быть создана папка **project-dist**
-async function checkDir(directory, folder) {
-  fs.readdir(directory, { withFileTypes: true }, (err, items) => {
-    if (err) return err;
-    if (!items.includes(folder)) fs.mkdir(path.join(directory, folder), err => err);
-  });
-}
-async function setInitialState() {
+// helper functions
+async function getDirItems(dir, dataType = '') {
+  const names = [];
+  const isFolders = [];
   try {
-    await checkDir(dir, resultFolder);
-    await fs.rm(path.join(dir, resultFolder, copyFolder), { recursive: true, force: true }, err => err);
-    setTimeout(() => {
-      checkDir(path.join(dir, resultFolder), copyFolder);
-    }, 50);
+    const items = await fs.readdir(dir, { withFileTypes: true });
+    items.map(el => {
+      if (dataType !== '') {
+        let ext = el.name.split('.');
+        if (ext[ext.length - 1] === dataType) {
+          names.push(el.name);
+          isFolders.push(el.isDirectory());
+        }
+      } else {
+        names.push(el.name);
+        isFolders.push(el.isDirectory());
+      }
+    });
+  } catch (err) {
+    return err;
+  }
+  return [names, isFolders];
+}
+
+async function writeData(readPath, writePath) {
+  try {
+    const data = await fs.readFile(readPath, { encoding: 'binary' });
+    await fs.writeFile(writePath, data, { encoding: 'binary', flag: 'a' });
   } catch (err) {
     return err;
   }
 }
 
-setInitialState();
-
-function fuseFiles(files, finalFile) {
-  const writeStream = fs.createWriteStream(finalFile, { encoding: 'binary', flags: 'a' });
-  files.forEach(el => {
-    const readStream = fs.createReadStream(el, { encoding: 'binary' });
-    readStream.on('readable', () => {
-      const data = readStream.read();
-      if (data) writeStream.write(`${data}`);
-    });
-  });
+// working logic
+async function setInitialState() {
+  try {
+    const [arr, types] = await getDirItems(dir);
+    if (arr.includes(resultFolder))
+      await fs.rm(path.join(dir, resultFolder), { recursive: true, force: true, maxRetries: 9 }, err => err);
+    await fs.mkdir(path.join(dir, resultFolder));
+  } catch (err) {
+    return err;
+  }
 }
 
-function getFiles(folderPath, pathArray, folderPathArray = []) {
-  let foldersName = [];
-  fs.readdir(folderPath, { withFileTypes: true }, (err, items) => {
-    if (err) return err;
-    for (let i = 0; i < items.length; i++) {
-      const elPath = path.join(folderPath, items[i].name);
-      if (items[i].isDirectory()) {
-        foldersName.push(items[i].name);
-        folderPathArray.push(elPath.split('assets')[1]);
-      } else {
-        fs.stat(elPath, (err, stats) => {
-          if (err) return err;
-          pathArray.push([elPath, stats.size]);
-        });
+async function replicate(item, copy) {
+  try {
+    await fs.mkdir(copy);
+    const [assetItems, assetTypes] = await getDirItems(item);
+    for (let i = 0; i < assetItems.length; i++) {
+      const itemPath = path.join(item, assetItems[i]);
+      const copyPath = path.join(copy, assetItems[i]);
+      if (!assetTypes[i]) await writeData(itemPath, copyPath);
+      if (assetTypes[i]) await replicate(itemPath, copyPath);
+    }
+  } catch (err) {
+    return err;
+  }
+}
+
+async function mergeStyles(folder) {
+  try {
+    const [styleItems, styleTypes] = await getDirItems(folder, 'css');
+    for (let i = 0; i < styleItems.length; i++) {
+      const itemPath = path.join(folder, styleItems[i]);
+      const resultPath = path.join(dir, resultFolder, 'style.css');
+      if (!styleTypes[i]) await writeData(itemPath, resultPath);
+      if (styleTypes[i]) await mergeStyles(itemPath);
+    }
+  } catch (err) {
+    return err;
+  }
+}
+
+async function makePage(folder) {
+  try {
+    const [compItems, compTypes] = await getDirItems(folder, 'html');
+    let template = path.join(dir, 'template.html');
+    let data = await fs.readFile(path.join(dir, 'template.html'), { encoding: 'binary' });
+    const resultPath = path.join(dir, resultFolder, 'index.html');
+    await writeData(template, resultPath);
+    for (let i = 0; i < compItems.length; i++) {
+      if (!compTypes[i]) {
+        const itemPath = path.join(folder, compItems[i]);
+        const tempData = await fs.readFile(itemPath, { encoding: 'binary' });
+        data = data.replace(`{{${compItems[i].replace('.html', '')}}}`, tempData);
       }
     }
-    if (foldersName.length > 0) {
-      for (let i = 0; i < foldersName.length; i++) {
-        const nextPath = path.join(folderPath, foldersName[i]);
-        getFiles(nextPath, pathArray);
-      }
-    }
-  });
+    await fs.writeFile(resultPath, data, { encoding: 'binary' });
+  } catch (err) {
+    return err;
+  }
 }
 
-function checkFinalSize(path, size) {
-  fs.stat(path, (err, stats) => {
-    if (err) return err;
-    if (stats) size.push(stats.size);
-    else size = undefined;
-  });
+async function build() {
+  try {
+    await setInitialState();
+    await replicate(path.join(dir, copyFolder), path.join(dir, resultFolder, copyFolder));
+    await mergeStyles(path.join(dir, stylesPath));
+    await makePage(path.join(dir, componentFolder));
+  } catch (err) {
+    return err;
+  }
 }
-// Файл **style.css** должен содержать стили собранные из файлов папки **styles**
-// В папке **project-dist** должны находиться файлы **index.html** и **style.css**
-function checkStyles(pathFolder) {
-  let styleArray = [];
-  let stylesSize = 0;
-  let finalStyleSize = [];
-
-  getFiles(pathFolder, styleArray);
-  checkFinalSize(path.join(dir, resultFolder, 'style.css'), finalStyleSize);
-  
-  setTimeout(() => {
-    styleArray.forEach(el => {
-      stylesSize += el[1];
-    });
-    if (finalStyleSize === undefined || stylesSize !== finalStyleSize[0]) {
-      console.log('rewrite styles');
-      fs.unlink(path.join(dir, resultFolder, 'style.css'), err => err);
-      fuseFiles(
-        styleArray.map(el => el[0]),
-        path.join(dir, resultFolder, 'style.css')
-      );
-    }
-  }, 50);
-}
-checkStyles(path.join(dir, stylesPath));
-
-function rewrite(array) {
-  array.forEach(el => {
-    let readPath = el;
-    let writePath = el.split('assets');
-    writePath = path.join(writePath[0], resultFolder, copyFolder, writePath[1]);
-    const writeStream = fs.createWriteStream(writePath, { encoding: 'binary', flags: 'w' });
-    const readStream = fs.createReadStream(readPath, { encoding: 'binary' });
-    readStream.on('readable', () => {
-      const data = readStream.read();
-      if (data) writeStream.write(`${data}`);
-    });
-  });
-}
-
-// В папке **project-dist** должна находиться папка **assets** являющаяся точной копией папки **assets**
-function checkAssets(pathFolder) {
-  let fileArray = [];
-  let folderArray = [];
-
-  getFiles(pathFolder, fileArray, folderArray);
-  setTimeout(() => {
-    folderArray.forEach(el => {
-      checkDir(path.join(dir, resultFolder, copyFolder), el);
-    });
-  }, 150);
-  setTimeout(() => {
-    rewrite(fileArray.map(el => el[0]));
-  }, 300);
-}
-checkAssets(path.join(dir, copyFolder));
-
-// Файл **index.html** должен содержать разметку являющуюся результатом замены шаблонных тегов в файле **template.html**
-// Запись в шаблон содержимого любых файлов кроме файлов с расширением **.html** является ошибкой
-// Исходный файл **template.html** не должен быть изменён в ходе выполнения скрипта
-// В папке **project-dist** должны находиться файлы **index.html** и **style.css**
-function checkHtml(pathFolder) {
-  let files = [];
-  const pattern = path.join(dir, 'template.html');
-  let htmlString;
-  const readStream = fs.createReadStream(pattern, { encoding: 'binary' });
-  readStream.on('readable', () => {
-    const data = readStream.read();
-    if (data) htmlString = data;
-  });
-
-  getFiles(pathFolder, files);
-  setTimeout(() => {
-    files = files.map(el => {
-      if (el[0].indexOf('.html') === el[0].length - 5) return el[0];
-    });
-    files.forEach(el => {
-      let fileName;
-      if (el.includes('\\')) {
-        const tempArr = el.split('\\');
-        fileName = tempArr[tempArr.length - 1].split('.')[0];
-      } else {
-        const tempArr = el.split('/');
-        fileName = tempArr[tempArr.length - 1].split('.')[0];
-      }
-      let readPath = el;
-      const readStream = fs.createReadStream(readPath, { encoding: 'binary' });
-      readStream.on('readable', () => {
-        const data = readStream.read();
-        if (data) htmlString = htmlString.replace(`{{${fileName}}}`, `${data}`);
-      });
-    });
-    setTimeout(() => {
-      let writePath = path.join(dir, resultFolder, 'index.html');
-      const writeStream = fs.createWriteStream(writePath, { encoding: 'binary', flags: 'w' });
-      writeStream.write(htmlString);
-    }, 50);
-  }, 100);
-}
-checkHtml(path.join(dir, componentFolder));
+build();
